19336831f1f456cb753a8508f8e28db143b7e4b1
yupeiyu <yupeiyu@users.noreply.github.com>
2017-04-16 12:53:16 +0800

    Update Ninenine.java

--- src/qahw/Ninenine.java
+++ src/qahw/Ninenine.java
@@ -4,11 +4,14 @@ public class Ninenine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//橫的印，一行印完，換行
+		
 		for(int j=1;j<=9;j++){
 			for(int i=1;i<=9;i++){
 				int sum=i*j;
-				System.out.print(i+"*"+j+"="+sum+"\t"); ///t=空格 給我去旁邊一點
-				
+				System.out.print(i+"*"+j+"="+sum+"\t"); // \t=空格 給我去旁邊一點
+				//同一行
 			}
 			System.out.println( );//換行
 		}
