820d5abeed3a98e606d3f74da9ba715bfccf9006
yupeiyu <yupeiyu@users.noreply.github.com>
2017-04-16 13:05:00 +0800

    運算子&^|~

---
+++ src/qahw/Bitwise.java
@@ -0,0 +1,25 @@
+package hw;
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        int i = 13;
+		        int j = 12;
+		 
+		        System.out.println("變數值...");
+		        System.out.println(" i = " + i);
+		        System.out.println(" j = " + j);
+		 
+		        System.out.println("位元運算...");
+		        System.out.println(" i & j = " + (i & j));
+		        System.out.println(" i | j = " + (i | j));
+		        System.out.println(" i ^ j = " + (i ^ j));
+		        System.out.println(" ~i = " + (~i));  //全部0改成1，變成原數值負數-1
+		        System.out.println(" ~j = " + (~j));  //
+		    }
+		
+	}
+
+
