0c305019d1496c7ca25c5d41516171ec4e123b37
yupeiyu <yupeiyu@users.noreply.github.com>
2017-04-16 12:51:46 +0800

    九九乘法表

---
+++ src/qahw/Ninenine.java
@@ -0,0 +1,17 @@
+package hw;
+
+public class Ninenine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int j=1;j<=9;j++){
+			for(int i=1;i<=9;i++){
+				int sum=i*j;
+				System.out.print(i+"*"+j+"="+sum+"\t"); ///t=空格 給我去旁邊一點
+				
+			}
+			System.out.println( );//換行
+		}
+	}
+
+}
